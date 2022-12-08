2cdf66f40c6388bdcc31e711b0ae175783f81b51
phinny01 <phinehas.fuachie20@gmail.com>
2022-12-08 00:15:11 -0500

    Master

--- Screens/Homescreen.tsx
+++ Screens/Homescreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Component } from 'react';
+import { Component, useState, useEffect } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -8,7 +8,7 @@ import {
   Button,
 } from 'react-native';
 import TimeTableView, { genTimeBlock } from 'react-native-timetable';
-import list_class_assignments from 
+
 
 // type Item = {
 //   class_assignments_due_today: Array<any>,
@@ -20,78 +20,8 @@ import list_class_assignments from
 //   body: string;
 //   userId: number;
 // };
-
-const GET_DETAIL_VIEW = (evt) => {
-  const course_code = (evt["title"])
-  //const [data, setData] = useState(null)
-
-  fetch('https://RemoteLoathsomeVoxels.donaldechefu1.repl.co/class_assignments_due/'+course_code)
-    .then(response => response.json())
-    .then(responseData => {
-      setData(responseData);
-    })
-    .catch(error => {
-      console.error(error);
-    });
-
-const events_data = [
-  {
-    title: "Math",
-    startTime: genTimeBlock("MON", 9),
-    endTime: genTimeBlock("MON", 10, 50),
-    location: "Classroom 403",
-    extra_descriptions: ["Kim", "Lee"],
-  },
-  {
-    title: "Math",
-    startTime: genTimeBlock("WED", 9),
-    endTime: genTimeBlock("WED", 10, 50),
-    location: "Classroom 403",
-    extra_descriptions: ["Kim", "Lee"],
-  },
-  {
-    title: "Physics",
-    startTime: genTimeBlock("MON", 11),
-    endTime: genTimeBlock("MON", 11, 50),
-    location: "Lab 404",
-    extra_descriptions: ["Einstein"],
-  },
-  {
-    title: "Physics",
-    startTime: genTimeBlock("WED", 11),
-    endTime: genTimeBlock("WED", 11, 50),
-    location: "Lab 404",
-    extra_descriptions: ["Einstein"],
-  },
-  {
-    title: "Mandarin",
-    startTime: genTimeBlock("TUE", 9),
-    endTime: genTimeBlock("TUE", 10, 50),
-    location: "Language Center",
-    extra_descriptions: ["Chen"],
-  },
-  {
-    title: "Japanese",
-    startTime: genTimeBlock("FRI", 9),
-    endTime: genTimeBlock("FRI", 10, 50),
-    location: "Language Center",
-    extra_descriptions: ["Nakamura"],
-  },
-  {
-    title: "Club Activity",
-    startTime: genTimeBlock("THU", 9),
-    endTime: genTimeBlock("THU", 10, 50),
-    location: "Activity Center",
-  },
-  {
-    title: "Club Activity",
-    startTime: genTimeBlock("FRI", 13, 30),
-    endTime: genTimeBlock("FRI", 14, 50),
-    location: "Activity Center",
-  },
-];
-
 export default class App extends Component {
+
   numOfDays: number;
   pivotDate: any;
   timetableRef: any;
@@ -104,9 +34,22 @@ export default class App extends Component {
   scrollViewRef = (ref) => {
     this.timetableRef = ref;
   };
-
+  
   onEventPress = (evt) => {
-    Alert.alert("onEventPress", JSON.stringify(evt));
+    const course_code = evt["title"]
+    let data = {};
+
+  
+    fetch('https://RemoteLoathsomeVoxels.donaldechefu1.repl.co/class_assignments_due/' + course_code )
+      .then(response => response.json())
+      .then(responseData => {
+        data = responseData;
+        Alert.alert("onEventPress", JSON.stringify(data));
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    
   };
 
   render() {
@@ -126,12 +69,12 @@ export default class App extends Component {
             locale="en-US"
           />
           
-          <Button 
+          {/* <Button 
             // style={styles.Button}
             title="Add Assignment"
             onPress={newAssignment}
             color="#841584"
-          />
+          /> */}
         </View>
       </SafeAreaView>
     );
@@ -157,4 +100,63 @@ const styles = StyleSheet.create({
     padding: 10,
     marginLeft: 5,
   },
-})};
+});
+
+
+const events_data = [
+  {
+    title: "CSCI136-02",
+    startTime: genTimeBlock("MON", 9),
+    endTime: genTimeBlock("MON", 10, 50),
+    location: "Classroom 403",
+    extra_descriptions: ["Kim", "Lee"],
+  },
+  {
+    title: "CSCI136-02",
+    startTime: genTimeBlock("WED", 9),
+    endTime: genTimeBlock("WED", 10, 50),
+    location: "Classroom 403",
+    extra_descriptions: ["Kim", "Lee"],
+  },
+  {
+    title: "CSCI101-01",
+    startTime: genTimeBlock("MON", 11),
+    endTime: genTimeBlock("MON", 11, 50),
+    location: "Lab 404",
+    extra_descriptions: ["Einstein"],
+  },
+  {
+    title: "CSCI101-01",
+    startTime: genTimeBlock("WED", 11),
+    endTime: genTimeBlock("WED", 11, 50),
+    location: "Lab 404",
+    extra_descriptions: ["Einstein"],
+  },
+  {
+    title: "MATH101-01",
+    startTime: genTimeBlock("TUE", 9),
+    endTime: genTimeBlock("TUE", 10, 50),
+    location: "Language Center",
+    extra_descriptions: ["Chen"],
+  },
+  {
+    title: "ENGW102-03",
+    startTime: genTimeBlock("FRI", 9),
+    endTime: genTimeBlock("FRI", 10, 50),
+    location: "Language Center",
+    extra_descriptions: ["Nakamura"],
+  },
+  {
+    title: "MATH101-01",
+    startTime: genTimeBlock("THU", 9),
+    endTime: genTimeBlock("THU", 10, 50),
+    location: "Activity Center",
+  },
+  {
+    title: "MATH101-01",
+    startTime: genTimeBlock("FRI", 13, 30),
+    endTime: genTimeBlock("FRI", 14, 50),
+    location: "Activity Center",
+  },
+];
+
